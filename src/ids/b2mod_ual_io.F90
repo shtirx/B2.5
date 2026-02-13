a58c42ae8af4fc02bc48c2389e6ac3504dab5171
Xavier Bonnin <Xavier.Bonnin@iter.org>
2026-02-13 18:09:07 +0100

    Lengthening string for Eirene Git hash

--- src/ids/b2mod_ual_io.F90
+++ src/ids/b2mod_ual_io.F90
@@ -10244,8 +10244,8 @@ contains
 #  ifdef B25_EIRENE
     integer p
     character*8 eirene_version
-    character*32 Eirene_git_version
-    character*32 get_Eir_hash
+    character*40 Eirene_git_version
+    character*40 get_Eir_hash
 #  endif
 #  ifdef AMNS
     type (amns_handle_type) :: amns
